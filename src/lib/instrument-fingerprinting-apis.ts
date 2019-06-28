0e54133dbe00e40c0c3f2699c4ecd3326f59cac3
eugenioemmolo <44060066+eugenioemmolo@users.noreply.github.com>
2019-06-28 11:18:13 +0200

    Update instrument-fingerprinting-apis.ts

--- src/lib/instrument-fingerprinting-apis.ts
+++ src/lib/instrument-fingerprinting-apis.ts
@@ -23,25 +23,23 @@ export function instrumentFingerprintingApis({
 
   // Access to navigator properties
   var navigatorProperties = ["platform", "product",
-  "productSub", "userAgent", "javaEnabled", "hardwareConcurrency"
+  "productSub", "userAgent", "hardwareConcurrency"
   ];
   navigatorProperties.forEach(function (property) {
   instrumentObjectProperty(window.navigator, "Navigator", property);
   });
 
-  var documentProperties = ["addEventListener", "appendChild", "baseURI",
-      "removeChild", "URL", "scripts", "createElement", "readyState"
+  var documentProperties = ["addEventListener", "scripts",
+                            "createElement", "readyState"
   ];
   documentProperties.forEach(function (property) {
   instrumentObjectProperty(window.document, "Document", property);
   });
   
   instrumentObject(Math, "Math", {
-  'propertiesToInstrument': ["abs", "exp", "log", "log10", "log2", "max", "min", "pow",
-  "random", "sign", "sqrt", "acos", "acosh", "asin", "asinh",
-  "atan", "atanh", "cbrt", "ceil", "clz32", "cos", "cosh",
-  "expm1", "floor", "fround", "hypot", "imul", "round", "sin",
-  "sinh", "tan", "tanh", "toSource", "trunc"]
+  'propertiesToInstrument': ["abs", "max", "min", "pow",
+  "random",  "atan", "ceil", "floor", "round", 
+  "asin", ]
   });
  
   instrumentObject(Worker.prototype, "Worker");
